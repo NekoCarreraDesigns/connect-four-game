27bab7a50d98740c4a69e0df3295545cd17787fa
NekoCarreraDesigns <denvermaas@gmail.com>
2022-02-09 11:51:36 -0700

    fixed display issues bug

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ Yellow = (255, 255, 0)
 
 Row_Count = 6
 Column_Count = 7
-Even = 0
-Odd = 1
 
 
 def create_board():
@@ -63,7 +61,7 @@ def draw_board(board):
     for c in range(Column_Count):
         for r in range(Row_Count):
             pygame.draw.rect(screen, Blue, (c*SquareSize, r *
-                             SquareSize+SquareSize, SquareSize, SquareSize))
+                                            SquareSize+SquareSize, SquareSize, SquareSize))
             pygame.draw.circle(screen, Black, (int(
                 c*SquareSize+SquareSize/2), int(r*SquareSize+SquareSize+SquareSize/2)), radius)
 
@@ -71,10 +69,10 @@ def draw_board(board):
         for r in range(Row_Count):
             if board[r][c] == 1:
                 pygame.draw.circle(screen, Red, (int(
-                    c*SquareSize+SquareSize/2), int(r*SquareSize+SquareSize/2)), radius)
+                    c*SquareSize+SquareSize/2), height-int(r*SquareSize+SquareSize/2)), radius)
             elif board[r][c] == 2:
                 pygame.draw.circle(screen, Yellow, (int(
-                    c*SquareSize+SquareSize/2), int(r*SquareSize+SquareSize/2)), radius)
+                    c*SquareSize+SquareSize/2), height-int(r*SquareSize+SquareSize/2)), radius)
 
     pygame.display.update()
 
@@ -132,20 +130,19 @@ while not game_over:
                         screen.blit(label, (40, 10))
                         game_over = True
 
-                else:
-                    posx = event.pos[0]
-                    col = int(math.floor(posx/SquareSize))
+            else:
+                posx = event.pos[0]
+                col = int(math.floor(posx/SquareSize))
 
-                    if is_valid_location(board, col):
-                        row = get_next_open_row(board, col)
-                        drop_piece(board, row, col, 2)
+                if is_valid_location(board, col):
+                    row = get_next_open_row(board, col)
+                    drop_piece(board, row, col, 2)
 
-                        if winning_move(board, 2):
-                            label = myfont.render(
-                                "Player 1 wins!!, Yaaaayy!!", 2, Yellow)
-                            screen.blit(label, (40, 10))
-                            game_over = True
-                            break
+                    if winning_move(board, 2):
+                        label = myfont.render(
+                            "Player 2 wins!!, Yaaaayy!!", 2, Yellow)
+                        screen.blit(label, (40, 10))
+                        game_over = True
 
             print_board(board)
             draw_board(board)
